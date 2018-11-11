2e68b30854533d06c621e23a507434223f946a30
Sean Host <sean@host.com>
2018-11-11 20:49:52 +0000

    First swagger api

---
+++ BackEnd/improvements.py
@@ -0,0 +1,3 @@
+def alerts():
+    testData = [{'id': 1, 'message': "Get started and create your first improvement!"}]
+    return testData
---
+++ BackEnd/run.py
@@ -0,0 +1,8 @@
+import connexion
+
+import improvements
+
+if __name__ == "__main__":
+    app = connexion.App(__name__)
+    app.add_api('apiSpec.yaml')
+    app.run(port=8080)

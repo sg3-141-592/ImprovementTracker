43ec9e64f0f18e7cd9ff88d3117efe00ffb806ea
Sean Host <sean@host.com>
2018-11-14 23:56:35 +0000

    Bootstrap Vue work starting

--- BackEnd/improvements.py
+++ BackEnd/improvements.py
@@ -14,6 +14,7 @@ def dismissAlert(alertnum):
             alertsData.remove(alert)
     print(alertnum)
 
+
 ## ----------------------------------------------------------
 ## Objectives
 ## ----------------------------------------------------------
